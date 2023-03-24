ad507a6b44d53a396af24d6692bc8d0db0157d70
Ashish <meashish2020@gmail.com>
2023-03-24 18:31:43 +0545

    DeleteFile.Java

--- src/FileHandling/DeleteFile.java
+++ src/FileHandling/DeleteFile.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class DeleteFile {
 
     public static void main(String[] args) {
-        File file = new File("Newpage.txt");
+        File file = new File("Newpage.txt");  // Deleting the created Files
         if(file.delete()){
             System.out.println(file.getName()+"=Deleted");
         }else{
